c373770495e4a2882301e40c3c8908b3c503b36e
unknown <mateus.saraiva09@gmail.com>
2023-01-20 16:52:54 -0300

    create server | first commit server

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+
+import fastify from "fastify"
+import {PrismaClient} from "@prisma/client"
+import cors from "@fastify/cors"
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.register(cors, {})
+
+/*
+
+    Metodo HTTP: get, post, put, patch, delete
+
+*/
+
+app.get('/', async () => {
+
+     const  habits = await prisma.habit.findMany()
+    return habits
+})
+
+app.listen({
+    port:3333,
+}).then(() => {
+    console.log('HTTP Server running!')
+})
